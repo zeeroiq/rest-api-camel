c43c7f41133d5f361e384332153f833b18192bfe
Shrikant Vishwakarma <shrikantvishwakarma@outlook.com>
2023-04-15 13:16:09 +0530

    user service fix and sample http client requests

--- src/main/java/com/shri/camel/rest/RestApisCamelApplication.java
+++ src/main/java/com/shri/camel/rest/RestApisCamelApplication.java
@@ -3,7 +3,7 @@ package com.shri.camel.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.shri.camel")
 public class RestApisCamelApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/shri/camel/rest/api/service/impl/UserServiceImpl.java
+++ src/main/java/com/shri/camel/rest/api/service/impl/UserServiceImpl.java
@@ -4,10 +4,13 @@ import com.shri.camel.rest.api.model.User;
 import com.shri.camel.rest.api.repository.UserRepository;
 import com.shri.camel.rest.api.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
+import static com.shri.camel.rest.util.Utility.listOfNullProperties;
 
 @RequiredArgsConstructor
 @Service
@@ -23,5 +26,17 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public User findUserByEmailId(String emailId) {
+        return userRepository.findByEmailId(emailId).orElseThrow(RuntimeException::new);
+    }
+
+    @Override
+    public User addUser(User user) {
+        User dbUser = userRepository.findByEmailId(user.getEmailId()).orElseThrow(RuntimeException::new);
+        BeanUtils.copyProperties(user, dbUser, listOfNullProperties(user));
+        return userRepository.save(user);
     }
 }
